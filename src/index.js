e453881c1b488ba56dbfe48a1f58fb69a2e3332c
fahred <turik20121998@gmail.com>
2019-10-04 10:05:48 +0300

    v 1.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello bitch")
+import './style.scss';
+
+let apiKey = "f77919380546d1f6ef8015d53089ba0e";
+
+function processInp() {
+    let cityName = document.getElementById("exampleInputCity").value;
+    getWeatherParams(cityName);
+}
+
+document.getElementById("submit").onclick = processInp;
+
+function getWeatherParams(cityName) {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?`, {
+        params: {
+            q: cityName,
+            appid: apiKey
+        }
+    })
+        .then(function(resultParam) {
+            let weather = resultParam.data
+            fillWeather(weather);
+        })
+}
+
+function fillWeather(weather) {
+    document.getElementById("item0").src="http://openweathermap.org/img/wn/"+weather.weather[0].icon+"@2x.png";
+    document.getElementById("item1").innerText = weather.name+", "+weather.sys.country;
+    document.getElementById("item2").innerText = "Current weather: "+weather.weather[0].main+" ( "+weather.weather[0].description+" )";
+    document.getElementById("item3").innerText = "Temperature: "+(weather.main.temp - 273.15).toFixed(0)+"°C";
+    document.getElementById("item4").innerText = "Wind: "+weather.wind.speed+"m/s";
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
-const path = require('path')
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {main: './src/index.js'},
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader']
+                    })
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.pug',
+            filename: 'index.html',
+            inject: true
+        }),
+        new ExtractTextPlugin(
+            {filename: 'style.css'}
+        )
+    ]
+};
