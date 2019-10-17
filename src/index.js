b46b28824cf985374b853988cd7f20fd18d951bf
Arthur Fakhretdinov <turik20121998@gmail.com>
2019-10-17 15:55:12 +0300

    v4 final maybe

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './style.scss';
-
+const tempFunction = require('./template.pug');
+const err = "Введите город";
 let apiKey = "f77919380546d1f6ef8015d53089ba0e";
 
-function processInp() {
-  let cityName = document.getElementById("exampleInputCity").value;
-  getWeatherParams(cityName);
+function processInp(event) {
+  getWeatherParams(event.target[0].value);
 }
 
-document.getElementById("submit").onclick = processInp;
-//getWeatherParams('Ufa')
+document.getElementById("Form").addEventListener("submit", processInp);
+
 function getWeatherParams(cityName) {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?`, {
         params: {
@@ -18,26 +18,30 @@ function getWeatherParams(cityName) {
     })
         .then(function(resultParam) {
             let weather = resultParam.data;
-            console.log(weather);
             fillWeather(weather);
         })
         .catch(function(err) {
-            console.log(err.message);
+            //console.log(err.message);
             fillErr(err.message);
         })
 }
 
 function fillErr(message) {
-    document.getElementById("item0").src="";
-    document.getElementById("item1").innerText = "";
-    document.getElementById("item2").innerText = "";
-    document.getElementById("item3").innerText = message;
-    document.getElementById("item4").innerText = "";
+  document.getElementById("listOfWeather").innerHTML = tempFunction({
+    pic: "",
+    place: "",
+    clouds: "",
+    temp: message,
+    wind: ""
+  });
 }
+
 function fillWeather(weather) {
-    document.getElementById("item0").src="http://openweathermap.org/img/wn/"+weather.weather[0].icon+"@2x.png";
-    document.getElementById("item1").innerText = weather.name+", "+weather.sys.country;
-    document.getElementById("item2").innerText = "Current weather: "+weather.weather[0].main+" ( "+weather.weather[0].description+" )";
-    document.getElementById("item3").innerText = "Temperature: "+(weather.main.temp - 273.15).toFixed(0)+"°C";
-    document.getElementById("item4").innerText = "Wind: "+weather.wind.speed+"m/s";
+  document.getElementById("listOfWeather").innerHTML = tempFunction({
+    pic: "http://openweathermap.org/img/wn/"+weather.weather[0].icon+"@2x.png",
+    place: weather.name+", "+weather.sys.country,
+    clouds: "Current weather: "+weather.weather[0].main+" ( "+weather.weather[0].description+" )",
+    temp: "Temperature: "+(weather.main.temp - 273.15).toFixed(0)+"°C",
+    wind: "Wind: "+weather.wind.speed+"m/s"
+  });
 }
