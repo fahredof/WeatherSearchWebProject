0f3af1d548bb0b6ed5032bfa55acfd25ed00c5ac
fahredof <turik20121998@gmail.com>
2019-12-27 05:18:50 +0300

    add fetch tests

--- src/js/getWeatherParams.js
+++ src/js/getWeatherParams.js
@@ -7,11 +7,13 @@ export default async function getWeatherParams(city) {
     let response = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
     let dataByCity;
+    console.log(response.ok);
     if (response.ok) {
         dataByCity = await response.json();
         await renderWeather(dataByCity);
     } else {
         dataByCity = await response.json();
+        console.log(dataByCity);
         //dataByCity = {error: "The server responded with a status of " + response.status + response.message };
         await renderErrorMessage(dataByCity);
     }
