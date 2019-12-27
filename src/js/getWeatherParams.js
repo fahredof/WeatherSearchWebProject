3b50b8863e400876ae58d0d48e4c0cf178904fe3
fahredof <turik20121998@gmail.com>
2019-12-27 05:24:02 +0300

    refactor getWeather test

--- src/js/getWeatherParams.js
+++ src/js/getWeatherParams.js
@@ -5,16 +5,12 @@ const apiKey = "f77919380546d1f6ef8015d53089ba0e";
 
 export default async function getWeatherParams(city) {
     let response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
-    let dataByCity;
-    console.log(response.ok);
-    if (response.ok) {
-        dataByCity = await response.json();
-        await renderWeather(dataByCity);
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+            .then(data => data.json());
+    if (response.cod === 200) {
+        await renderWeather(response);
     } else {
-        dataByCity = await response.json();
-        console.log(dataByCity);
-        //dataByCity = {error: "The server responded with a status of " + response.status + response.message };
-        await renderErrorMessage(dataByCity);
+        await renderErrorMessage(response);
     }
+    return response;
 }
